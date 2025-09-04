5abce8aab844e80b4c99a59c91796cdbae526779
MinorMinion <urchin710@gmail.com>
2025-09-04 18:53:35 +0500

    2. here my first part of Context Api is done...... UserContext->UserContextProvider->SetData->GetData

--- 01/src/App.jsx
+++ 01/src/App.jsx
@@ -1,19 +1,33 @@
-import './App.css'
+import UserContextProvider from "./context/UserContextProvider";
+import Login from "./components/login";
+import Profile from "./components/profile";
+import "./App.css";
 
 function App() {
 
   return (
-    <>
-    <div>
+    < >
       <div>
-        <p>here is the Header section</p>
+        <div>
+          <p>here is the Header section</p>
+        </div>
       </div>
-      
-    </div>
-      <h1 className=' bg-blue-100 text-center justify-center align-center text-4xl p-4'>Start of My Portfolio</h1>
-      <p>i have installed Vite, imbeded Tailwind css, and connected to Git. All done till now and now my site is ready to start.</p>
+      <h1 className=" bg-blue-100 text-center justify-center align-center text-4xl p-4">
+        Start of My Portfolio
+      </h1>
+      <p>
+        i have installed Vite, imbeded Tailwind css, and connected to Git. All
+        done till now and now my site is ready to start.
+      </p>
+      <br /><hr /><br />
+      <UserContextProvider>
+        <Login />
+        <br /><hr /><br />
+        <Profile />
+        <br /><hr /><br />
+      </UserContextProvider>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
