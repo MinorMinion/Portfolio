e73d95b1e2d38220cbf480304813ecb487e8094e
MinorMinion <urchin710@gmail.com>
2025-09-04 12:52:19 +0500

    basic project setup Tailwind, Done.....

---
+++ 01/src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <div>
+      <div>
+        <p>here is the Header section</p>
+      </div>
+      
+    </div>
+      <h1 className=' bg-blue-100 text-center justify-center align-center text-4xl p-4'>Start of My Portfolio</h1>
+      <p>i have installed Vite, imbeded Tailwind css, and connected to Git. All done till now and now my site is ready to start.</p>
+    </>
+  )
+}
+
+export default App
